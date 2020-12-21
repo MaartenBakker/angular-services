b8425a27bc3f5e9a861f6218179c3e9e568b7230
Maarten Bakker <maartenbakker@gmail.com>
2020-12-21 12:47:01 +0100

    Service at app.module level, and injecting a Service into another Service.

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,14 +6,11 @@ import {AccountService} from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService,
-              private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
